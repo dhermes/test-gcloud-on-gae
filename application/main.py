4457b2f846ac8f65f38e5c38fbf2258a60a67ebe
Danny Hermes <daniel.j.hermes@gmail.com>
2014-12-30 13:31:02 -0800

    Customizing gcloud/ lib so all imports work.

--- application/main.py
+++ application/main.py
@@ -1,5 +1,7 @@
 import webapp2
 
+from gcloud import datastore
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
--- application/vendor/gcloud/__init__.py
+++ application/vendor/gcloud/__init__.py
@@ -1,5 +1,7 @@
 """GCloud API access in idiomatic Python."""
 
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('gcloud').version
+try:
+    from pkg_resources import get_distribution
+    __version__ = get_distribution('gcloud').version
+except ImportError:
+    __version__ = None
