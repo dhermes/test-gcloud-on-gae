8a850fb8b5676ca03e07c4f9dcfba5efb8c77b0a
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-22 15:08:37 -0800

    Changing to appengine_config.py to use darth vendor.
    See:
    https://github.com/jonparrott/Darth-Vendor
    
    Ran `$ darth-bootstrap` to get the `darth.py` script set up.

--- application/appengine_config.py
+++ application/appengine_config.py
@@ -1,16 +1,15 @@
 # H/T: www.simonmweber.com/2013/06/18/python-protobuf-on-app-engine.html
 
 import os
-import sys
 
+import darth
 # App Engine imports are higher up the import path.
 import google
+
+darth.vendor('vendor')
 
 # Add vendor/google/ to google namespace package so that
 # google.protobuf is imported.
 curr_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(curr_dir, 'vendor')
 google.__path__.append(os.path.join(vendor_dir, 'google'))
-
-# Add vendor path for imports.
-sys.path.insert(0, vendor_dir)
